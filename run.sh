82769b94fe4572b034083a0045943b711933a2ad
Tebi <bilguun.tuvshin@gmail.com>
2022-04-26 09:07:35 +0800

    added PosAPI service, restructured the repo

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ do
   DOCKER_COMMAND+=($arg)
 done
 
-docker-compose -p $PROJECT_NAME -f docker-compose.yml ${DOCKER_COMMAND[*]}
+docker-compose -p $PROJECT_NAME -f docker-compose.yml -f docker-compose.env.yml ${DOCKER_COMMAND[*]}
